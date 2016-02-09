919c6ba994ab27e6927067f7e7a91ff4bf0b5dbd
Bryan Patrick Coleman <totalknowledge@gmail.com>
2016-02-08 23:15:28 -0500

    Up and running with Cyclone for the Web Services.  Refs #1

--- backend.py
+++ backend.py
@@ -1,6 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-import os, sys, cgi, json, cPickle
+import os, sys, json, cPickle
+import cyclone.web
+
+from twisted.internet import reactor
+from twisted.python import log
 
 def getPickle(fp):
 	try:
@@ -12,77 +16,120 @@ def getPickle(fp):
 		return {"nextval":100,
 				"data":{}
 			   }
+
 def savePickle(fp, obj):
 	wfile = open(fp, 'wb')
 	wfile.write(cPickle.dumps(obj, 2))
 	wfile.close()
 
-# Make pickle_jar if it doesn't exist
-if not os.path.exists("pickle_jar"):
-	os.makedirs("pickle_jar")
+class MainHandler(cyclone.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world get")
+    def post(self):
+        self.write("Hello, world post")
+    def put(self):
+        self.write("Hello, world put")
+    def delete(self):
+        self.write("Hello, world delete")
+    def patch(self):
+        self.write("Hello, world patch")
 
-# Grab the REQUEST_METHOD collection and id if exists from the Request
-id = 0
-if 'PATH_INFO' in os.environ:
-	path_info = os.environ['PATH_INFO'].split('/')
-else:
-	path_info = ["",'default']
+class WebServiceHandler(cyclone.web.RequestHandler):
+    def get(self, path):
+		# Grab the REQUEST_METHOD collection and id if exists from the Request
+		id = 0
+		path_info = path.split('/')
 
-if len(path_info) > 1:
-	collection = path_info[1]
-	file_path = 'pickle_jar/' + collection + '.pickle'
-	open(file_path, 'a+').close()
+		collection = path_info[0]
+		file_path = 'pickle_jar/' + collection + '.pickle'
+		open(file_path, 'a+').close()
 
-if len(path_info) > 2:
-	id = path_info[2]
-	
-if 'REQUEST_METHOD' in os.environ:
-	request_method = os.environ['REQUEST_METHOD']
-else:
-	request_method = "GET"
+		if len(path_info) > 1:
+			id = path_info[1]
 
-if request_method == "GET":
-	if id:
-		result = getPickle(file_path)
-		try:
-			response_obj = {"data":{str(id):result["data"][str(id)]}}
-			status_code = '200 OK'
-		except:
-			status_code = '404 Not Found'
-	else:
-		response_obj = {"data":getPickle(file_path)["data"]}
-		status_code = '200 OK'
+		if id:
+			result = getPickle(file_path)
+			try:
+				response_obj = {"data":{str(id):result["data"][str(id)]}}
+				self.write(response_obj)
+			except:
+				self.set_status(404)
+		else:
+			response_obj = {"data":getPickle(file_path)["data"]}
+			self.write(response_obj)
 
-elif request_method == "PUT" or request_method == "POST":
-	save_obj = getPickle(file_path)
-	if id:
-		save_obj["data"][str(id)] = json.loads(sys.stdin.read())
-		status_code = '200 OK'
-	else:
-		id = save_obj["nextval"]
-		save_obj["data"][str(id)] = json.loads(sys.stdin.read())
-		save_obj["nextval"] = save_obj["nextval"] + 1
-		status_code = '201 Created'
-	response_obj = {"data":{str(id):save_obj["data"][str(id)]}}
-	savePickle(file_path, save_obj)
+    def post(self, path):
+		path_info = path.split('/')
 
-elif request_method == "DELETE":
-	if id:
+		collection = path_info[0]
+		file_path = 'pickle_jar/' + collection + '.pickle'
+		open(file_path, 'a+').close()
+
 		save_obj = getPickle(file_path)
-		try:
-			del save_obj["data"][str(id)]
-			savePickle(file_path, save_obj)
-			status_code = '204 No Content'
-		except:
-			status_code = '404 Not Found'
-	else:
-		savePickle(file_path, {"nextval":100,
-							   "data":{}
-							  })
-		status_code = '204 No Content'
+		id = save_obj["nextval"]
+		save_obj["data"][str(id)] = json.loads(self.request.body)
+		save_obj["nextval"] = save_obj["nextval"] + 1
+		self.set_status(201)
+		response_obj = {"data":{str(id):save_obj["data"][str(id)]}}
+		savePickle(file_path, save_obj)
+		self.write(response_obj)
 
-print "Status: %s\r" % status_code
-print "Content-type:application/json\r\n"
+    def put(self, path):
+		id=0
+		path_info = path.split('/')
+		collection = path_info[0]
+		file_path = 'pickle_jar/' + collection + '.pickle'
+		open(file_path, 'a+').close()
 
-if not (status_code == '204 No Content' or status_code == '404 Not Found'):
-	print json.dumps(response_obj)
+		if len(path_info) > 1:
+			id = path_info[1]
+
+		save_obj = getPickle(file_path)
+		save_obj["data"][str(id)] = json.loads(self.request.body)
+
+		response_obj = {"data":{str(id):save_obj["data"][str(id)]}}
+		savePickle(file_path, save_obj)
+		self.write(response_obj)
+
+    def delete(self, path):
+		id=0
+		path_info = path.split('/')
+
+		collection = path_info[0]
+		file_path = 'pickle_jar/' + collection + '.pickle'
+		open(file_path, 'a+').close()
+
+		if len(path_info) > 1:
+			id = path_info[1]
+
+		if id:
+			save_obj = getPickle(file_path)
+			try:
+				del save_obj["data"][str(id)]
+				savePickle(file_path, save_obj)
+				self.set_status(204)
+			except:
+				self.set_status(404)
+		else:
+			savePickle(file_path, {"nextval":100,
+								   "data":{}
+								  })
+			self.set_status(204)
+
+    def patch(self, path):
+        self.write("Not Implemented")
+
+if __name__ == "__main__":
+
+	# Make pickle_jar if it doesn't exist
+	if not os.path.exists("pickle_jar"):
+		os.makedirs("pickle_jar")
+
+	application = cyclone.web.Application([
+		(r"/", MainHandler),
+		(r"/ws/(.*)", WebServiceHandler)
+	])
+
+	log.startLogging(sys.stdout)
+	reactor.listenTCP(1234, application)
+	reactor.run()
