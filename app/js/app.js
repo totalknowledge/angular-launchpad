5f2bdd18b8814d36c14197810d14444ef776fbef
Bryan Patrick Coleman <totalknowledge@gmail.com>
2016-02-10 12:59:07 -0500

    Angular2 Framework added.  Refs #5

--- app/js/app.js
+++ app/js/app.js
@@ -1,39 +1,10 @@
-'use strict'
-
-var myApp = angular.module('rpApp', [])
-  .factory('rpFactory', ['$http', '$q', '$log',
-    function($http, $q, $log) {
-      return {
-        mmmm: 'value'
-      }
+System.config({
+  packages: {
+    app: {
+      format: 'register',
+      defaultExtension: 'js'
     }
-  ])
-  .controller('rpController', ['$log', '$scope', '$http',
-    function($log, $scope, $http) {
-      $scope.values = {};
-      $scope.values.banner = "This is a Value";
-      $scope.right = 8;
-	  $scope.left = 4;
-      
-      $scope.resizeLeft = function (direction) {
-		  if (direction == 'minus' && $scope.left>1) {
-			$scope.left--;
-			$scope.right++;
-		  } else if (direction == 'plus' && $scope.left<11) {
-			  $scope.right--;
-			  $scope.left++;
-		  }
-	  }
-	  $scope.sum = function (a, b) {
-		  return a + b;
-	  }
-	  $scope.addone = angular.bind(self, $scope.sum, 1);
-    }
-  ])
-  .filter('displayname', ['$log',
-    function($log) {
-      return function(returnValue, matrix) {
-        return _.find(matrix, {id:returnValue}).name;
-      };
-    }
-  ])
+  }
+});
+System.import('app/js/main')
+      .then(null, console.error.bind(console));
