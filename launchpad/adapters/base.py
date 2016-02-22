6ca1b6626585482a1d49197cea19627108d31d22
Tyson DeNio <tysondenio@gmail.com>
2016-02-22 09:24:30 -0500

    Add new data adapter abstraction

---
+++ launchpad/adapters/base.py
@@ -0,0 +1,24 @@
+from abc import ABCMeta, abstractmethod
+
+class BaseAdapter(object):
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def get_collection(self, collection, order_by = None, filter_by = None, page = None):
+        return []
+
+    @abstractmethod
+    def get_record(self, collection, id):
+        return {}
+
+    @abstractmethod
+    def set(self, collection, id):
+        return []
+
+    @abstractmethod
+    def delete_collection(self, collection):
+        return []
+
+    @abstractmethod
+    def delete_record(self, collection, id):
+        return []
