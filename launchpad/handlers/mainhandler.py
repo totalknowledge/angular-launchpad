f0969ad86eeb91a776c3c4063d75c3bbe71ec721
Bryan Patrick Coleman <totalknowledge@gmail.com>
2016-03-06 15:06:26 -0500

    Added mime types for common file endings, and allow for default file type when one is not provided.  Fixes #35

--- launchpad/handlers/mainhandler.py
+++ launchpad/handlers/mainhandler.py
@@ -10,14 +10,31 @@ class MainHandler(cyclone.web.RequestHandler):
 
     @staticmethod
     def get_file_path(path):
-        return os.path.join(settings.doc_dir, path)
+        path_segment = path.split('?',1)[0]
+        return os.path.join(settings.doc_dir, path_segment)
 
     def get(self, path):
-        if path.startswith('app/') or path.startswith('node_modules/'):
+        if path.startswith('app') or path.startswith('node_modules'):
+            self.mimetypes = {'html': 'text/html',
+                              'css': 'text/css',
+                              'js': 'application/javascript',
+                              'map': 'application/javascript',
+                              'ico': 'image/x-icon',
+                              'jpg': 'image/jpeg',
+                              'png': 'image/png',
+                              'gif': 'image/gif',
+                              'xml': 'application/xml'}
+            filetype = path.split('.')[-1]
+            if filetype not in self.mimetypes:
+                path = path + '.js'
+                filetype = 'js'
+            boo = filetype
             try:
                 with open(self.get_file_path(path), 'rb') as request_page:
                     self.write(request_page.read())
                     self.set_status(200)
+                    if filetype in self.mimetypes.keys():
+                        self.set_header("Content-Type", self.mimetypes[filetype])
             except Exception as e:
                 self.write('''<h1 style="font-family: Arial, Helvetica, sans-serif;
                            margin: 20px;">404 File Not Found</h1>''')
@@ -27,11 +44,12 @@ class MainHandler(cyclone.web.RequestHandler):
                 with open(self.get_file_path(path), 'rb') as request_page:
                     self.write(request_page.read())
                     self.set_status(200)
+                    self.set_header("Content-Type", "image/x-icon")
             except Exception as e:
                 self.write('''<h1 style="font-family: Arial, Helvetica, sans-serif;
                            margin: 20px;">404 File Not Found</h1>''')
                 self.set_status(404)
         else:
-            with open(self.get_file_path('index.html'), 'r') as request_page:
+            with open(self.get_file_path('index.html'), 'rb') as request_page:
                 self.write(request_page.read())
                 self.set_status(200)
