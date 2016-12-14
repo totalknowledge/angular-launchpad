920162b7627cc7a7d0e848ae0d134c9ceab6e377
Bryan Patrick Coleman <totalknowledge@gmail.com>
2016-12-14 14:11:52 -0500

    Fixed prints and imports.  Refs #44

--- launchpad/adapters/picklejar.py
+++ launchpad/adapters/picklejar.py
@@ -1,4 +1,4 @@
-import cPickle
+import pickle
 
 class PickleJar(object):
     """ Store persistant data in pickle files. """
--- launchpad/app.py
+++ launchpad/app.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 import os
 import sys
 
@@ -9,7 +10,8 @@ if not os.path.exists(os.path.join(settings.doc_dir, "pickle_jar")):
         os.makedirs(os.path.join(settings.doc_dir, "pickle_jar"))
 
 # Late import
-from handlers import MainHandler, WebServiceHandler
+from launchpad.handlers import MainHandler
+from launchpad.handlers import WebServiceHandler
 
 app = tornado.web.Application([
         (r"/api/v0/(.*)", WebServiceHandler),
