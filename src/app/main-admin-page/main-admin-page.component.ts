61bbba4b425bd8bc5be6604961fff15ea305a3c8
Bryan Patrick Coleman <totalknowledge@gmail.com>
2020-09-03 14:39:29 -0400

    Security and file updates.

--- src/app/main-admin-page/main-admin-page.component.ts
+++ src/app/main-admin-page/main-admin-page.component.ts
@@ -28,8 +28,8 @@ export class MainAdminPageComponent implements OnInit {
   edit: boolean;
   users:Array<any>;
 
-  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
-  @ViewChild(MatSort, {static: false}) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(service:CollectionService, auth:AuthService) {
     this.service = service;
